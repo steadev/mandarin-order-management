828e816f15fb9be9d8aa86f9079772344e417b68
MyungJoo Lee <audwn0326@gmail.com>
2022-08-31 23:38:28 +0900

    feat: syntax error

--- src/screens/OrderList/OrderList.tsx
+++ src/screens/OrderList/OrderList.tsx
@@ -9,7 +9,7 @@ const OrderList = () => {
     { id: 1, name: '이명주', phone: '01029541187', address: '경기도 파주시 교하로 100', order: '감귤 1', amount: 10000, delivered: true },
     { id: 2, name: '이명주', phone: '01029541187', address: '경기도 파주시 교하로 100', order: '감귤 2', amount: 20000, delivered: true },
     { id: 3, name: '이명주', phone: '01029541187', address: '경기도 파주시 교하로 100', order: '감귤 3', amount: 30000, delivered: true },
-    { id: 4, name: '이명주', phone: '01029541187', address: '경기도 파주시 교하로 100', order: '감귤 4', amount: 40000, delivered: true}
+    { id: 4, name: '이명주', phone: '01029541187', address: '경기도 파주시 교하로 100', order: '감귤 4', amount: 40000, delivered: true }
   ]
   // order table
   // add
