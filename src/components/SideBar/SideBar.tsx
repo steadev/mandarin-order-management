b28b948cebc67a30c0c68d6cb8b9064d4b61bb25
MyungJoo Lee <audwn0326@gmail.com>
2022-09-20 09:33:13 +0900

    chore: set prettier

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,26 +1,38 @@
-import { faChartBar, faHomeAlt, faRedoAlt, IconDefinition } from '@fortawesome/free-solid-svg-icons'
+import {
+  faChartBar,
+  faHomeAlt,
+  faRedoAlt,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './SideBar.module.css'
 const SideBar = () => {
   const menus: {
-    icon: IconDefinition;
-    path: string;
-  }[] = [{
-    icon: faHomeAlt,
-    path: ''
-    }, {
-    icon: faChartBar,
-      path:'stats'
-      }]
+    icon: IconDefinition
+    path: string
+  }[] = [
+    {
+      icon: faHomeAlt,
+      path: '',
+    },
+    {
+      icon: faChartBar,
+      path: 'stats',
+    },
+  ]
 
-  return <div className={styles.container}>
-    <div className={styles.relaod}>
-      <FontAwesomeIcon icon={faRedoAlt} />
+  return (
+    <div className={styles.container}>
+      <div className={styles.relaod}>
+        <FontAwesomeIcon icon={faRedoAlt} />
+      </div>
+      <div className={styles.menu}>
+        {menus.map((menu) => (
+          <FontAwesomeIcon icon={menu.icon} />
+        ))}
+      </div>
     </div>
-    <div className={styles.menu}>
-      {menus.map((menu) => <FontAwesomeIcon icon={menu.icon} />)}
-    </div>
-</div>
+  )
 }
 
 export default SideBar
