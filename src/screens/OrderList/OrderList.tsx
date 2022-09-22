d03becc67384977cb6167b622ef0620bf409bfe3
MyungJoo Lee <audwn0326@gmail.com>
2022-09-22 22:28:50 +0900

    feat: order list header

--- src/screens/OrderList/OrderList.tsx
+++ src/screens/OrderList/OrderList.tsx
@@ -2,13 +2,16 @@ import Header from './components/Header'
 import OrderTable from './components/OrderTable'
 import SearchBar from './components/SearchBar'
 import SortingTab from './components/SortingTab'
+import styles from './OrderList.module.css'
 
 const OrderList = () => {
   return (
-    <div>
-      <Header />
-      <SearchBar />
-      <SortingTab />
+    <div className={styles.container}>
+      <div className={styles.headerContainer}>
+        <Header />
+        <SearchBar />
+        <SortingTab />
+      </div>
       <OrderTable />
     </div>
   )
--- src/screens/OrderList/components/Header.tsx
+++ src/screens/OrderList/components/Header.tsx
@@ -1,5 +1,30 @@
+import {
+  faFileExport,
+  faFileImport,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styles from './Header.module.css'
 const Header = () => {
-  return <div></div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>주문 목록</div>
+      <div className={styles.buttonListWrapper}>
+        <button className={styles.button}>
+          <FontAwesomeIcon icon={faFileImport} />
+          불러오기
+        </button>
+        <button className={styles.button}>
+          <FontAwesomeIcon icon={faFileExport} />
+          내보내기
+        </button>
+        <button className={styles.addButton}>
+          <FontAwesomeIcon icon={faPlus} />
+          주문 생성
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Header
