e025f395c392d2f8914f1b30e641aba8f31d369d
MyungJoo Lee <audwn0326@gmail.com>
2022-09-24 00:00:04 +0900

    feat: add calendar style

--- src/screens/OrderList/components/SearchBar.tsx
+++ src/screens/OrderList/components/SearchBar.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
 import styles from './SearchBar.module.css'
 
 const SearchBar = () => {
